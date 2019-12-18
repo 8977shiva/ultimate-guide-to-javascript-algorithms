007251ff5a1a9e14523ef3df322f92dba7007b94
8977shiva <b.skreddy1997@gmail.com>
2019-12-19 02:46:59 +0530

    day 2

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -6,6 +6,17 @@ e.g vowelsCounter('anehizxcv') // will return 3
 
 function vowelsCounter(text) {
     // Code goes here
+    
+    const arr=['a','i','o','u','e']
+    var counter=0;
+       for(let i=0; i<text.length; i++){
+           
+               if(arr.includes(text[i].toLowerCase()))
+               {
+                               counter++;
+               }
+       }
+return counter;
 }
 
 
